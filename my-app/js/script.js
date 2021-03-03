bddfe6f880809a740b8248c36404b3c85583735b
Vito Gentile <gentilevito1076@gmail.com>
2021-03-03 16:21:27 +0100

    INDEX:HTML: Preparazione Strutura + SCRIPT: Chiamata Axios

--- my-app/js/script.js
+++ my-app/js/script.js
@@ -1,5 +1,21 @@
 new Vue({
   // Qui sotto dichiaro in quale container Vue verrà visualizzato:
-  el: '#container',
+  el: '#root',
+  data: {
+  arrayDiscs: []
+},
+
+mounted() {
+  const self = this;
+  console.log('ho montato l\'app');
+
+    axios.get('http://localhost/php-ajax-dischi/my-app/app/server.php')
+    .then(function(resp) {
+      console.log('risposta vue', resp.data);
+      self.arrayDiscs = resp.data;
+      console.log(self.arrayDiscs);
+    });
+}
+
 });
 Vue.config.devtools = true;
