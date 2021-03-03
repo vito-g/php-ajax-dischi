c5d4c4653261cf4879f1f853480425229f3a3a8f
Vito Gentile <gentilevito1076@gmail.com>
2021-03-03 15:44:50 +0100

    SCRIPT.JS: Instanziamento Vue + DevTools

---
+++ my-app/js/script.js
@@ -0,0 +1,7 @@
+// <!-- INSTANZIARE Vew: -->
+var app = new Vue({
+  // Qui sotto dichiaro in quale container Veu verrà visualizzato:
+  el: '#root',
+});
+Vue.config.devtools = true;
+//<!-- INSTANZIARE Vew: -->
