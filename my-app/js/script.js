18ded47f619c18981e1e261af9107dcb7da0a0d0
Vito Gentile <gentilevito1076@gmail.com>
2021-03-03 16:37:00 +0100

    Aggiornamento

--- my-app/js/script.js
+++ my-app/js/script.js
@@ -3,19 +3,19 @@ new Vue({
   el: '#root',
   data: {
   arrayDiscs: []
-},
+  },
 
-mounted() {
-  const self = this;
-  console.log('ho montato l\'app');
+  mounted() {
+    const self = this;
+    console.log('ho montato l\'app');
 
-    axios.get('http://localhost/php-ajax-dischi/my-app/app/server.php')
-    .then(function(resp) {
-      console.log('risposta vue', resp.data);
-      self.arrayDiscs = resp.data;
-      console.log(self.arrayDiscs);
-    });
-}
+      axios.get('http://localhost/php-ajax-dischi/my-app/app/server.php')
+      .then(function(resp) {
+        console.log('risposta vue', resp.data);
+        self.arrayDiscs = resp.data;
+        console.log(self.arrayDiscs);
+      });
+  }
 
 });
 Vue.config.devtools = true;
